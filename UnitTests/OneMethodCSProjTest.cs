0b41900fc40c0b71bed2d74713a86368f51afe5b
Michal Klein <kleinmichalgm@gmail.com>
2023-01-18 21:32:20 +0100

    create OneMethodCSProjAnalyzer unit test

---
+++ UnitTests/OneMethodCSProjTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// OneMethodCSProjAnalyzerTest for testing OneMethodCSProj Scope of analysis via OneMethodCSProjAnalyzer.
+    /// </summary>
+    [TestClass]
+    public class OneMethodCSProjTest
+    {
+
+    }
+}
